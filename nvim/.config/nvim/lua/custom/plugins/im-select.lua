ee7622afcb676f860010314cd7def8f3abe90831
정우혁 <whjeong@ajd.co.kr>
2026-05-11 08:27:06 +0900

    nvim: im-select 플러그인 추가

---
+++ nvim/.config/nvim/lua/custom/plugins/im-select.lua
@@ -0,0 +1,14 @@
+local function gh(repo) return 'https://github.com/' .. repo end
+
+vim.pack.add {
+  gh 'keaising/im-select.nvim',
+}
+
+require('im_select').setup {
+  default_command = 'macism',
+  default_im_select = 'com.apple.keylayout.ABC',
+  set_default_events = { 'VimEnter', 'InsertLeave', 'CmdlineLeave' },
+  set_previous_events = { 'InsertEnter' },
+  keep_quiet_on_no_binary = false,
+  async_switch_im = true,
+}
