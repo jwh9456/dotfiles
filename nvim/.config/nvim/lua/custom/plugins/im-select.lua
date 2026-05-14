6a4c639a1447995813be7860220dbd7cf6d1a1ff
정우혁 <whjeong@ajd.co.kr>
2026-05-14 09:15:38 +0900

    nvim: 로그레벨, im-select설정 변경

--- nvim/.config/nvim/lua/custom/plugins/im-select.lua
+++ nvim/.config/nvim/lua/custom/plugins/im-select.lua
@@ -7,7 +7,7 @@ vim.pack.add {
 require('im_select').setup {
   default_command = 'macism',
   default_im_select = 'com.apple.keylayout.ABC',
-  set_default_events = { 'VimEnter', 'InsertLeave', 'CmdlineLeave' },
+  set_default_events = { 'InsertLeave', 'CmdlineLeave' },
   set_previous_events = { 'InsertEnter' },
   keep_quiet_on_no_binary = false,
   async_switch_im = true,
