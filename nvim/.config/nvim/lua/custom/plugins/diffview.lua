d6f2fc910e64f47ae69efba70015480a5bbbab87
정우혁 <whjeong@ajd.co.kr>
2026-07-15 10:09:22 +0900

    nvim: 설정 변경

--- nvim/.config/nvim/lua/custom/plugins/diffview.lua
+++ nvim/.config/nvim/lua/custom/plugins/diffview.lua
@@ -7,5 +7,10 @@ vim.pack.add {
 
 vim.keymap.set('n', '<leader>gD', '<cmd>DiffviewOpen<cr>', { desc = 'Diffview' })
 vim.keymap.set('n', '<leader>gH', '<cmd>DiffviewFileHistory %<cr>', { desc = 'File History' })
+vim.keymap.set('n', '<leader>gp', function()
+  vim.ui.input({ prompt = 'Base: ', default = 'origin/master' }, function(base)
+    if base and base ~= '' then vim.cmd('DiffviewOpen ' .. base .. '...HEAD') end
+  end)
+end, { desc = 'PR Diff vs base' })
 
 require('diffview').setup {}
--- nvim/.config/nvim/lua/custom/plugins/kotlin.lua
+++ nvim/.config/nvim/lua/custom/plugins/kotlin.lua
@@ -14,8 +14,20 @@ vim.lsp.handlers['textDocument/rename'] = function(err, result, ctx, config)
   return rename_handler(err, result, ctx, config)
 end
 
+-- kotlin-lsp는 Homebrew(JetBrains/utils/kotlin-lsp)로 설치한다(mason 아님).
+-- brew의 `kotlin-lsp`는 intellij-server 런처 심볼릭 링크라 `--stdio`로 호출한다.
+-- (lspconfig 기본 cmd는 mason의 `intellij-server`라서 반드시 덮어써야 함)
 vim.lsp.config('kotlin_lsp', {
+  cmd = { 'kotlin-lsp', '--stdio' },
   flags = {
-    debounce_text_changes = 0,
+    -- 입력마다 didChange를 보내면 FIR 분석 엔진의 lazy-resolve 레이스가 잦아진다.
+    -- 디바운스를 둬서 서버 부하/에러를 줄인다(0 -> 150ms).
+    debounce_text_changes = 150,
   },
+  on_init = function(client)
+    -- 시맨틱 토큰 비활성화: 현재 빌드(LS-262.4739.0)의 LSKotlinSemanticTokensProvider가
+    -- FIR resolve 중 예외를 폭주시킨다. 하이라이트는 treesitter가 담당.
+    -- 서버 측에서 수정되면 이 on_init 블록만 제거하면 된다.
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
 })
